03caa2e034941b6df7a8395af492adcc2861f185
lzk50136 <178550136@qq.com>
2019-01-26 16:57:05 +0800

    Initial commit

---
+++ src/main/java/com/example/notes/SpringBootNotesApplication.java
@@ -0,0 +1,14 @@
+package com.example.notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootNotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootNotesApplication.class, args);
+    }
+
+}
+
