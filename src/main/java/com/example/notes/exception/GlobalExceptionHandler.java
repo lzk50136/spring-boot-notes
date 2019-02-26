669f02056350a33d1771188b10ea7c98fb88f3f9
asd178550136 <asd178550136@163.com>
2019-02-26 10:11:13 +0800

    2019年2月26日10点11分

--- src/main/java/com/example/notes/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/notes/exception/GlobalExceptionHandler.java
@@ -4,15 +4,11 @@ import com.example.notes.util.ApiResult;
 import com.example.notes.util.ApiResultUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
-import java.util.List;
 
 /**
  * 全局异常拦截处理器
@@ -33,30 +29,9 @@ public class GlobalExceptionHandler {
      */
     @ExceptionHandler(Exception.class)
     public ApiResult exception(Exception e) {
-        return resultFormat(e);
-    }
-
-    /**
-     * 处理实体字段校验不通过异常
-     */
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ApiResult validationError(MethodArgumentNotValidException ex) {
-        BindingResult result = ex.getBindingResult();
-        List<FieldError> fieldErrorList = result.getFieldErrors();
-        StringBuilder builder = new StringBuilder();
-        for (FieldError error : fieldErrorList) {
-            builder.append(error.getDefaultMessage());
-        }
-        return ApiResultUtil.error(10006, "失败", builder.toString());
-    }
-
-    /**
-     * 返回统一格式
-     */
-    private <T extends Throwable> ApiResult resultFormat(T t) {
-        logger.error(t.getMessage());
-        logger.error(Arrays.toString(t.getStackTrace()));
-        return ApiResultUtil.error(10099, "失败", t.getMessage());
+        logger.error(e.getMessage());
+        logger.error(Arrays.toString(e.getStackTrace()));
+        return ApiResultUtil.error(10099, "失败", e.getMessage());
     }
 
 }
